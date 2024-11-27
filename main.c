5a8ff29fb9d26cc447fb43e0bccde485bcc82131
enaa <hossnabensalem2@gmail.com>
2024-11-27 12:05:09 +0100

    ajouté fonction afficher une tache

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct{
      char description[100];
      char priorite[10];
      Date_echeance date;
- }Tache;
+}Tache;
      Tache ts[100];
      int Taille =0;
     
@@ -21,8 +21,7 @@ void ajouter(){
     printf("donner une description:");
     scanf(" %[^\n]%*c",ts[Taille].description);
     int choix;
-    do
-    {
+  do{
         printf("entre la priorite(1-high 2-low):");
         scanf("%d",&choix);
         switch (choix)
@@ -38,24 +37,24 @@ void ajouter(){
     }while(choix!=1 && choix!=2);
   do{
      printf("Entrer le jour (1 a 31):");
-    scanf("%d",&ts[Taille].date.jour); 
-        if(ts[Taille].date.jour>=1 && ts[Taille].date.jour<=31 ){ 
+     scanf("%d",&ts[Taille].date.jour); 
+     if(ts[Taille].date.jour>=1 && ts[Taille].date.jour<=31 ){ 
+     }
+     else{
+         printf("le jour est invalide, \n");
         }
-        else{
-            printf("le jour est invalide, \n");
-        }
-        }while (ts[Taille].date.jour<1 || ts[Taille].date.jour>31);
-do{
-    printf("donner le mois:");
-    scanf("%d",&ts[Taille].date.mois);
+     }while (ts[Taille].date.jour<1 || ts[Taille].date.jour>31);
+  do{
+     printf("donner le mois:");
+     scanf("%d",&ts[Taille].date.mois);
     if(ts[Taille].date.mois>=1 && ts[Taille].date.mois<=12){
     }
     else{
         printf("le mois est invalide  \n");
     }
-}while (ts[Taille].date.mois<1 || ts[Taille].date.mois>12);
+    }while (ts[Taille].date.mois<1 || ts[Taille].date.mois>12);
 
-do{
+  do{
     printf("donner l'annee:");
     scanf("%d",&ts[Taille].date.annee);
     if(ts[Taille].date.annee<=2050){
@@ -64,9 +63,25 @@ do{
     else{
         printf("annee est invalide \n");
     }
-}while (ts[Taille].date.annee>2050);
+    }while (ts[Taille].date.annee>2050);
     Taille++;   
     printf("la tache a ete ajouter avec succes");
+}
+void afficher()
+{
+    if(Taille==0){
+        printf("aucun taches disponibles!!");
+        return;
+    }
+    int i;
+    for(i=0;i<Taille;i++){
+    printf("le titre est :%s\n",ts[i].titre);
+    printf("la description est :%s\n",ts[i].description);
+    printf("la priorite est :%s\n",ts[i].priorite);
+    printf("le jour est :%d\n",ts[i].date.jour);
+    printf("le mois est :%d\n",ts[i].date.mois);  
+    printf("l'annee est :%d\n",ts[i].date.annee);
+    }
 }
 void menu(){
         printf("==============================\n");
@@ -74,7 +89,8 @@ void menu(){
         printf("==============================\n");
         printf("Entrer :\n");
         printf("1. pour ajouter une tache\n");
-        printf("2. quitter \n");
+        printf("2. pour afficher la liste des taches\n");
+        printf("3. quitter \n");
         
 }
 int main()
@@ -88,7 +104,10 @@ int main()
         case 1:
         ajouter();
         break;
-        case 2 : printf("au revoir!\n");
+        case 2: 
+        afficher();
+        break;
+        case 3 : printf("au revoir!\n");
         exit(0);
         default:
         printf("option invalide\n"); 
